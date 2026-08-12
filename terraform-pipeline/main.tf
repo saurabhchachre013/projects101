046535473a329c9adb29b89166d123d0a7774fd0
Saurabh Chachre <saurabchachre@gmail.com>
2026-08-12 21:01:32 +0530

    made changes

--- terraform-pipeline/main.tf
+++ terraform-pipeline/main.tf
@@ -7,6 +7,6 @@ resource "aws_s3_bucket" "developbucket" {
   bucket = "dev-saurabh-terraform"
 }
 
-resource "aws_s3_bucket" "developbucket" {
+resource "aws_s3_bucket" "developbucket1" {
   bucket = "dev-saurabh-terraform-101"
 }
